6831f9901eeb9b8c503e5342dbff64f3d818196e
Woojiyeong <woojiyeong.dev@gmail.com>
2026-04-14 10:11:21 +0900

    TodoListApp addTodo()

--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,10 +1,23 @@
-import Button from "./Button.jsx"
+import { useState } from 'react';
+import Button from "./Button.jsx";
 
-export default function TodoAdder() {
+export default function TodoAdder({ addTodo }) {
+    const [inputTodo, setInputTodo] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(!inputTodo.trim()) return; // 빈칸이면 바로 return
+        addTodo(inputTodo.trim()); //input에 있는 text value를 할 일 내용으로 추가하자
+        setInputTodo('')
+    }
     return (
         <>
-            <form className="todo__form">
-                <input type="text" className="todo__input" placeholder="할 일을 입력하세요." />
+            <form className="todo__form" onSubmit={handleSubmit}>
+                <input 
+                type="text" 
+                className="todo__input" 
+                placeholder="할 일을 입력하세요."
+                value={inputTodo}
+                onChange={(event)=> setInputTodo(event.target.value)} />
                 <Button type="submit" className="todo__button todo__button--add">Add</Button>
             </form>
         </>
