415322bcdfbbbded8b5a1dfbad2f7a2b2a8ec2a1
Woojiyeong <woojiyeong.dev@gmail.com>
2026-04-14 09:28:03 +0900

    TodoListApp h1 -> TodoHeader, form -> TodoAdder, li-> TodoItem, ul -> TodoList

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,14 @@
+import Checkbox from "./CheckBox.jsx";
+import Button from "./Button.jsx";
+
+export default function TodoItem() {
+    return (
+        <>
+            <li className="todo__item todo__item-complete">
+                <Checkbox type="checkbox" className="todo__check" name="" id="1" />
+                <Button className="todo__button todo__button--edit">🖋️</Button>
+                <Button className="todo__button todo__button--delete">🗑️</Button>
+            </li>
+        </>
+    )
+}
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import TodoItemEmpty from "./TodoItemEmpty.jsx"
+import TodoItem from "./TodoItem.jsx"
+
+export default function TodoList() {
+    return (
+        <>
+            <ul className="todo__list">
+                <TodoItemEmpty />
+                <TodoItem />
+            </ul>
+        </>
+    )
+}
