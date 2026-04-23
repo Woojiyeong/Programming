d5a36ba9e160e3ed3421305c3b1cf2da9bd6b827
Woojiyeong <woojiyeong.dev@gmail.com>
2026-04-23 14:38:38 +0900

    TodoListApp editTodo()

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,18 +1,47 @@
+import { useState } from "react";
 import Checkbox from "./CheckBox.jsx";
 import Button from "./Button.jsx";
 
-export default function TodoItem({ todo, toggleTodo, deleteTodo }) {
+export default function TodoItem({ todo, toggleTodo, deleteTodo, editTodo }) {
+    const [isEditing, setIsEditing] = useState(false); //수정중인지 아닌지
+    const [editText, setIsEditText] = useState(todo.text); // 수정중인 text
+
+    function handleEditClick(){
+        if(!isEditing) { //edit 시작
+            setIsEditing(true);
+            setIsEditText(todo.text);
+        }else{ //edit 끝
+            const trimmedText = editText.trim();
+            //빈칸이 아니고, 이전 text가 아닐때
+            if(trimmedText !== "" && trimmedText !== todo.text){
+                editTodo(todo.id, trimmedText);
+            }
+            setIsEditing(false);
+        }
+    }
     return (
         <>
             {/* todo.isCompleted가 true가  " todo__Item--complete", false "" */}
             <li className={`todo__item${todo.isCompleted ? " todo__item--complete" : ""}`}>
+                {!isEditing &&
                 <Checkbox
                     id={todo.id}
                     checked={todo.isCompleted}
                     onChange={() => toggleTodo(todo.id)}
-                >
-                    {todo.text}</Checkbox>
-                <Button className="todo__button todo__button--edit">🖋️</Button>
+                >{todo.text}</Checkbox>
+                }
+                {isEditing &&
+                    <input 
+                        type="text"
+                        className="todo__input--edit"
+                        value={editText}
+                        onChange={(event) => setIsEditText(event.target.value)}
+                        onKeyDown={(event) => {if(event.key === "Enter") handleEditClick()}}
+                        autoFocus
+                        />
+                }
+                <Button className="todo__button todo__button--edit" 
+                    onClick={handleEditClick} >{isEditing?"💾":"🖋️"}</Button>
                 <Button 
                     className="todo__button todo__button--delete"
                      onClick={() => deleteTodo(todo.id)}
